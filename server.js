bae621e569a0a46b2c4001c96f5b3927509ad2cf
Victor <victor.lieu@gmail.com>
2021-06-02 00:14:13 -0600

    initial commit, added boilerplate code

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const tmi = require('tmi.js');
+
+
+const client = new tmi.Client({
+    connection: {
+        reconnect: true
+    },
+    identity: {
+		username: process.env.TWITCH_BOT_USERNAME,  // username of twitch bot
+		password: process.env.TWITCH_0AUTH_TOKEN    // OAuth token
+	},
+	channels: [ process.env.TWITCH_STREAMER_USERNAME ] // username of twitch channel/streamer
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+    // TODO: handle channel point redemption actions
+});
