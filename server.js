3e7e602b126341657b6c75cae03802c55db00aa3
Victor <victor.lieu@gmail.com>
2021-06-02 00:15:28 -0600

    fixed typo

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const client = new tmi.Client({
     },
     identity: {
 		username: process.env.TWITCH_BOT_USERNAME,  // username of twitch bot
-		password: process.env.TWITCH_0AUTH_TOKEN    // OAuth token
+		password: process.env.TWITCH_OAUTH_TOKEN    // OAuth token
 	},
 	channels: [ process.env.TWITCH_STREAMER_USERNAME ] // username of twitch channel/streamer
 });
